118e7d181f831d306118fdae7515a15895010204
brjazgin <k.brjazgin@ftc.ru>
2015-10-08 13:50:53 +0600

    % test

--- src/main/java/ru/kosdev/imageviewer/client/ImageViewer.java
+++ src/main/java/ru/kosdev/imageviewer/client/ImageViewer.java
@@ -14,8 +14,8 @@ public class ImageViewer implements EntryPoint {
     public void onModuleLoad() {
         final ImageViewerPanel viewer = ViewerFactory.createImageViewerPanel();
         viewer.setUrl("http://icdn.lenta.ru/images/2015/10/02/14/20151002142531780/online_a57bbfb74077da336bed0e562542942c.jpg");
-        viewer.setWidth("250");
-        viewer.setHeight("250");
+        viewer.setWidth("25%");
+        viewer.setHeight("25%");
 
         Button fitButton = new Button("Fit");
         fitButton.addClickHandler(new ClickHandler() {
